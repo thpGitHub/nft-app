7dceefbb62deed8445267658e09851c4912eedb0
thierry <thierrypoupon@hotmail.com>
2022-12-20 10:54:03 +0100

    :tada: init project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Our Equilibrium collection', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/Our Equilibrium collection/i);
+  expect(element).toBeInTheDocument();
 });
