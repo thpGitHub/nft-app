9d53978f4465e22585c0d49092c05268d18ec8a6
thierry <thierrypoupon@hotmail.com>
2022-12-21 22:28:38 +0100

    :test_tube: run success test for testing circleCI

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App'
 
 test('renders Our Equilibrium collection', () => {
   render(<App />)
-  const element = screen.getByText(/Our Equilibrium collection22/i)
+  const element = screen.getByText(/Our Equilibrium collection/i)
   expect(element).toBeInTheDocument()
 })
