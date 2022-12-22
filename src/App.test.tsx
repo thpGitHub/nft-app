a53c40b0f528cb59cba048b008464519ec3aa59b
thierry <thierrypoupon@hotmail.com>
2022-12-22 12:55:39 +0100

    :test_tube: :bug: fix render of app.test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import {render, screen} from '@testing-library/react'
 import App from './App'
 
-test('renders Our Equilibrium collection', () => {
+test('renders Formation TypeScript', () => {
   render(<App />)
-  const element = screen.getByText(/Our Equilibrium collection/i)
+  const element = screen.getByText(/Formation TypeScript/i)
   expect(element).toBeInTheDocument()
 })
